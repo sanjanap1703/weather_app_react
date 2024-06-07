1ea079306bcd9825ba6a83f0c81ea9ff0993344b
sanjana pawar <psanjana077@gmail.com>
2024-06-07 22:41:34 +0530

    github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // vite.config.js
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/weather_app_react/',  // Set the base path to match your GitHub Pages URL
   build: {
-    outDir: 'dist'  // Ensure this matches the directory gh-pages expects
+    outDir: 'dist'
   }
-};
+});
