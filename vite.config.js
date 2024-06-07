3c645556614329ecc6fc7627f576ab3256e76c51
sanjana pawar <psanjana077@gmail.com>
2024-06-07 22:37:02 +0530

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  build: {
+    outDir: 'dist'  // Ensure this matches the directory gh-pages expects
+  }
+};
